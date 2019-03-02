820dba8367e447e5bd91e352074ee88eb35ab919
Herbert Poul <herbert@poul.at>
2019-03-03 00:52:24 +0200

    change release key file permission.

--- release.sh
+++ release.sh
@@ -15,6 +15,7 @@ if ! test -e ./git-buildnumber.sh ; then
     chmod +x git-buildnumber.sh
 fi
 
+chmod 400 _tools/deploy-key/github-deploy-key
 ssh-keygen -F github.com > /dev/null || (mkdir -p ~/.ssh && echo "github.com,192.30.253.113 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts)
 
 buildnumber=`GIT_SSH_COMMAND='ssh -i _tools/deploy-key/github-deploy-key' GIT_PUSH_REMOTE='git@github.com:hpoul/finalyzer-game.git' ./git-buildnumber.sh`
