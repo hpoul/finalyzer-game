e4693a0157c0db271c5b5f6f28c64f02c99c9ffa
Herbert Poul <herbert@poul.at>
2019-03-03 03:07:59 +0200

    install flutter depending on host platform.

--- _tools/install_flutter.sh
+++ _tools/install_flutter.sh
@@ -5,12 +5,17 @@ set -e
 set -x
 
 FLUTTER_VERSION=v1.2.1-stable
+FLUTTER_PLATFORM=macos
 
-curl -o flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_${FLUTTER_VERSION}.zip
+platform="$(uname -s)"
+case "${platform}" in
+    Linux*)     FLUTTER_PLATFORM=linux;;
+    Darwin*)    FLUTTER_PLATFORM=mac;;
+    *)          echo "Unknown platform ${platform} exit 1 ;;
+esac
+curl -o flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/${FLUTTER_PLATFORM}/flutter_${FLUTTER_PLATFORM}_${FLUTTER_VERSION}.zip
 
 unzip flutter.zip | tail
 
 export PATH=flutter/bin:$PATH
-
-
 
