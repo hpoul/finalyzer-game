fb22cbeb535a2b5ed84d5cedf5a8d5d2a49096cd
Herbert Poul <herbert@poul.at>
2019-03-03 04:14:13 +0200

    flutter download for linux uses tar.xz

--- _tools/install_flutter.sh
+++ _tools/install_flutter.sh
@@ -8,18 +8,23 @@ DEPS=${DEPS} # must be defined by environment.
 
 FLUTTER_VERSION=v1.2.1-stable
 FLUTTER_PLATFORM=macos
+FLUTTER_EXT=zip
 
 platform="$(uname -s)"
 case "${platform}" in
-    Linux*)     FLUTTER_PLATFORM=linux;;
+    Linux*)     FLUTTER_PLATFORM=linux ; FLUTTER_EXT=tar.xz ;;
     Darwin*)    FLUTTER_PLATFORM=mac;;
     *)          echo "Unknown platform ${platform}" ; exit 1 ;;
 esac
 
 pushd ${DEPS}
-curl -o flutter.zip https://storage.googleapis.com/flutter_infra/releases/stable/${FLUTTER_PLATFORM}/flutter_${FLUTTER_PLATFORM}_${FLUTTER_VERSION}.zip
+curl -o flutter.${FLUTTER_EXT} https://storage.googleapis.com/flutter_infra/releases/stable/${FLUTTER_PLATFORM}/flutter_${FLUTTER_PLATFORM}_${FLUTTER_VERSION}.${FLUTTER_EXT}
 
-unzip ${DEPS}/flutter.zip | tail
+if test "${FLUTTER_EXT}" = "zip" ; then
+    unzip flutter.zip | tail
+else
+    tar xf flutter.${FLUTTER_EXT} | tail
+fi
 
 popd
 
