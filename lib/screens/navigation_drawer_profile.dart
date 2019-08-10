463d238b485271b44085bb395f668a8292d7914c
Herbert Poul <herbert@poul.at>
2019-08-10 23:20:27 +0200

    fix a couple of API dto bugs, show build numer in drawer.

--- lib/screens/navigation_drawer_profile.dart
+++ lib/screens/navigation_drawer_profile.dart
@@ -9,6 +9,7 @@ import 'package:anlage_app_game/utils/deps.dart';
 import 'package:anlage_app_game/utils/dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:package_info/package_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 final _logger = Logger('app.anlage.game.screens.navigation_drawer_profile');
@@ -25,10 +26,19 @@ class NavigationDrawerProfile extends StatelessWidget {
             stream: _api.loginState,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return Column(
-                  children: const <Widget>[
-                    Text('Error while fetching user information.'),
-                  ],
+                return Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('Error while fetching user information.'),
+                      RaisedButton(
+                        child: const Text('Retry'),
+                        onPressed: () {
+                          _api.triggerUpdateUserInfo();
+                        },
+                      ),
+                    ],
+                  ),
                 );
               }
               if (!snapshot.hasData) {
@@ -39,162 +49,190 @@ class NavigationDrawerProfile extends StatelessWidget {
                 );
               }
 
-              final displayName = snapshot?.data?.userInfo?.displayName ?? '';
-              final userType = snapshot?.data?.userInfo?.userType ?? GameUserType.User;
-              return Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Ink(
-                    color: FinalyzerTheme.colorPrimary,
-                    child: InkWell(
-                      onTap: () {
-                        _logger.info('Tapped on profile image.');
-                        Navigator.of(context).pushNamed(ProfileEdit.ROUTE_NAME);
-//                        CloudMessagingUtil.instance.requestPermission();
-                      },
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        padding: const EdgeInsets.all(16).copyWith(top: MediaQuery.of(context).padding.top + 16),
-                        child: DefaultTextStyle(
-                          style: Theme.of(context).primaryTextTheme.body1,
-                          textAlign: TextAlign.right,
-                          child: !snapshot.hasData
-                              ? Container()
-                              : Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: <Widget>[
-                                        Container(
-                                          margin: const EdgeInsets.only(bottom: 16),
-                                          constraints: BoxConstraints.tightFor(width: 128, height: 128),
-                                          child: AvatarWithEditIcon(
-                                            snapshot?.data?.avatarUrl,
-                                            radius: 64,
-                                          ),
-                                        ),
-                                      ] +
-                                      (displayName == ''
-                                          ? [
-                                              Text(
-                                                'Hello Anonymous Investor!',
-                                                style: TextStyle(fontWeight: FontWeight.bold),
-                                              ),
-                                              const Text(
-                                                'Tell us a bit about you to get listed in the leaderboard!',
-                                              ),
-                                            ]
-                                          : [Text('Hello $displayName!')]) +
-                                      (userType == GameUserType.User
-                                          ? []
-                                          : [
-                                              Text(
-                                                '(${convertGameUserTypeToJson(userType)})',
-                                                style: Theme.of(context).textTheme.caption,
-                                              )
-                                            ]),
-                                ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: LayoutBuilder(
-                      builder: (context, constraints) => SingleChildScrollView(
-                        child: Container(
-                          constraints: BoxConstraints(
-                            minHeight: constraints.maxHeight,
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  ListTile(
-                                    leading: Icon(Icons.assignment),
-                                    title: const Text('Completed Turns:'),
-                                    trailing: Text(
-                                      '${snapshot.data?.userInfo?.statsTotalTurns ?? '?'}',
-                                      style: Theme.of(context).textTheme.display1,
-                                    ),
-                                  ),
-                                  ListTile(
-                                    leading: Icon(
-                                      Icons.thumb_up,
-                                      color: Colors.green,
-                                    ),
-                                    title: const Text('Correct Answers:'),
-                                    trailing: Text(
-                                      '${snapshot.data?.userInfo?.statsCorrectAnswers ?? '?'}',
-                                      style: Theme.of(context).textTheme.display1,
-                                    ),
-                                  ),
-                                  Divider(),
-                                ],
-                              ),
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  ListTile(
-                                    leading: Icon(Icons.send),
-                                    title: const Text('Challenge a friend.'),
-                                    onTap: () {
-                                      Navigator.of(context).pushNamed(ChallengeInvite.ROUTE_NAME);
-                                    },
-                                  ),
-                                  ListTile(
-                                    leading: Icon(Icons.view_list),
-                                    title: const Text('Challenges'),
-                                    onTap: () {
-                                      Navigator.of(context).pushNamed(ChallengeList.ROUTE_NAME);
-                                    },
-                                  ),
-                                  ListTile(
-                                    leading: Icon(Icons.format_list_numbered),
-                                    title: const Text('Leaderboard'),
-                                    onTap: () {
-                                      Navigator.of(context).pushNamed(LeaderboardList.ROUTE_NAME);
-                                    },
-                                  ),
-                                  Divider(),
-                                  ListTile(
-                                    leading: Icon(Icons.email),
-                                    title: const Text('How can we improve? Problems?'),
-                                    subtitle: const Text('We love to hear from you at hello@anlage.app'),
-                                    onTap: () {
-                                      DialogUtil.openFeedback(origin: 'drawer');
-                                    },
-                                  ),
-                                  ListTile(
-                                    leading: Icon(Icons.link),
-                                    title: const Text('By https://Anlage.App/'),
-                                    subtitle: const Text('Track&Analyse your portfolio.'),
-                                    onTap: () async {
-                                      const url = 'https://anlage.app/?utm_source=marketcap-game';
-                                      await DialogUtil.launchUrl(url);
-                                    },
-                                    onLongPress: () async {
-                                      _logger.severe('TEST Crash Stuff', Error(), StackTrace.current);
-                                      const url = 'https://anlage.app/?utm_source=marketcap-game';
-                                      if (await canLaunch(url)) {
-                                        await launch(url, forceSafariVC: false);
-                                      } else {
-                                        _logger.severe('Unable to launch url $url');
-                                      }
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              );
+              return FutureBuilder<PackageInfo>(
+                  future: PackageInfo.fromPlatform(),
+                  builder: (context, packageInfoSnapshot) {
+                    return NavigationDrawerContent(
+                      loginType: snapshot.data,
+                      packageInfo: packageInfoSnapshot.data,
+                    );
+                  });
             }),
       ),
+    );
+  }
+}
+
+class NavigationDrawerContent extends StatelessWidget {
+  const NavigationDrawerContent({Key key, @required this.loginType, @required this.packageInfo})
+      : assert(loginType != null),
+        super(key: key);
+
+  final LoginState loginType;
+  final PackageInfo packageInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    final displayName = loginType.userInfo?.displayName ?? '';
+    final userType = loginType.userInfo?.userType ?? GameUserType.User;
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Ink(
+          color: FinalyzerTheme.colorPrimary,
+          child: InkWell(
+            onTap: () {
+              _logger.info('Tapped on profile image.');
+              Navigator.of(context).pushNamed(ProfileEdit.ROUTE_NAME);
+//                        CloudMessagingUtil.instance.requestPermission();
+            },
+            child: Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.all(16).copyWith(top: MediaQuery.of(context).padding.top + 16),
+              child: DefaultTextStyle(
+                style: Theme.of(context).primaryTextTheme.body1,
+                textAlign: TextAlign.right,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 16),
+                          constraints: BoxConstraints.tightFor(width: 128, height: 128),
+                          child: AvatarWithEditIcon(
+                            loginType.avatarUrl,
+                            radius: 64,
+                          ),
+                        ),
+                      ] +
+                      (displayName == ''
+                          ? [
+                              Text(
+                                'Hello Anonymous Investor!',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              const Text(
+                                'Tell us a bit about you to get listed in the leaderboard!',
+                              ),
+                            ]
+                          : [Text('Hello $displayName!')]) +
+                      (userType == GameUserType.User
+                          ? []
+                          : [
+                              Text(
+                                '(${convertGameUserTypeToJson(userType)})',
+                                style: Theme.of(context).textTheme.caption,
+                              )
+                            ]),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Flexible(
+          child: LayoutBuilder(
+            builder: (context, constraints) => SingleChildScrollView(
+              child: Container(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.assignment),
+                          title: const Text('Completed Turns:'),
+                          trailing: Text(
+                            '${loginType.userInfo?.statsTotalTurns ?? '?'}',
+                            style: Theme.of(context).textTheme.display1,
+                          ),
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.thumb_up,
+                            color: Colors.green,
+                          ),
+                          title: const Text('Correct Answers:'),
+                          trailing: Text(
+                            '${loginType.userInfo?.statsCorrectAnswers ?? '?'}',
+                            style: Theme.of(context).textTheme.display1,
+                          ),
+                        ),
+                        Divider(),
+                      ],
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.send),
+                          title: const Text('Challenge a friend.'),
+                          onTap: () {
+                            Navigator.of(context).pushNamed(ChallengeInvite.ROUTE_NAME);
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.view_list),
+                          title: const Text('Challenges'),
+                          onTap: () {
+                            Navigator.of(context).pushNamed(ChallengeList.ROUTE_NAME);
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.format_list_numbered),
+                          title: const Text('Leaderboard'),
+                          onTap: () {
+                            Navigator.of(context).pushNamed(LeaderboardList.ROUTE_NAME);
+                          },
+                        ),
+                        Divider(),
+                        ListTile(
+                          leading: Icon(Icons.email),
+                          title: const Text('How can we improve? Problems?'),
+                          subtitle: const Text('We love to hear from you at hello@anlage.app'),
+                          onTap: () {
+                            DialogUtil.openFeedback(origin: 'drawer');
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.link),
+                          title: const Text('By https://Anlage.App/'),
+                          subtitle: const Text('Track&Analyse your portfolio.'),
+                          onTap: () async {
+                            const url = 'https://anlage.app/?utm_source=marketcap-game';
+                            await DialogUtil.launchUrl(url);
+                          },
+                          onLongPress: () async {
+                            _logger.severe('TEST Crash Stuff', Error(), StackTrace.current);
+                            const url = 'https://anlage.app/?utm_source=marketcap-game';
+                            if (await canLaunch(url)) {
+                              await launch(url, forceSafariVC: false);
+                            } else {
+                              _logger.severe('Unable to launch url $url');
+                            }
+                          },
+                        ),
+                        ...(packageInfo == null
+                            ? []
+                            : [
+                                Text(
+                                  'MarketCap Game V${packageInfo.version}-${packageInfo.buildNumber}',
+                                  style: TextStyle(color: Colors.black26, fontSize: 12),
+                                ),
+                                const SizedBox(height: 8),
+                              ]),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
